b94999743ab6dacfa6351fab87ea5d618ea8c4a3
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-04-09 11:25:18 +0300

    generics

--- cats/views.py
+++ cats/views.py
@@ -1,19 +1,71 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+# from rest_framework.views import APIView
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+
+# class APICat(APIView):
+#     def get(self, request):
+#         cats = Cat.objects.all()
+#         serializer = CatSerializer(cats, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = CatSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['GET', 'POST'])
+# def cat_list(request):
+#     if request.method == 'POST':
+#         # Проверяем, является ли входящие данные списком
+#         if isinstance(request.data, list):
+#             # Если это список, используем параметр many=True
+#             serializer = CatSerializer(data=request.data, many=True)
+#         else:
+#             # Если это одиночный объект
+#             serializer = CatSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     cats = Cat.objects.all()
+#     serializer = CatSerializer(cats, many=True)
+#     return Response(serializer.data)
+
+
+# @api_view(['GET', 'POST'])  # Применили декоратор и указали разрешённые методы
+# def hello(request):
+#     # По задумке, в ответ на POST-запрос нужно вернуть JSON с теми данными, 
+#     # которые получены в запросе.
+#     # Для этого в объект Response() передаём словарь request.data. 
+#     if request.method == 'POST':
+#         return Response({'message': 'Получены данные', 'data': request.data})
+
+#     # В ответ на GET-запрос нужно вернуть JSON
+#     # Он тоже будет создан из словаря, переданного в Response()
+#     return Response({'message': 'Это был GET-запрос!'})
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from cats.views import cat_list
+from cats.views import CatList, CatDetail
 
 urlpatterns = [
-   path('cats/', cat_list),
+    path('cats/', CatList.as_view()),
+    path('cats/<int:pk>/', CatDetail.as_view()),
 ]
 
 
+# from django.urls import path
+# from cats.views import APICat
+
+# # from cats.views import cat_list
+
+# urlpatterns = [
+#    # path('cats/', cat_list),
+#    path('cats/', APICat.as_view()),
+# ]
